99aca433df3368435fe7eef8320580ec586f9d8d
Miles Possing <milespossing@microsoft.com>
2024-03-08 15:29:23 +0000

    Adding basics

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -40,6 +40,18 @@
             inputs.home-manager.nixosModules.default
           ];
         };
+      };
+      homeConfigurations."mpossing" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+          ./home/wsl.nix
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
       };
     };
 }
