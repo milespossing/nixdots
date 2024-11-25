c2f797bf1a839fcb75f3d5b7bf8fc1fc378a8213
Miles Possing <git@possing.tech>
2024-11-24 18:21:24 -0600

    Updates

--- home/dots/emacs/init.el
+++ home/dots/emacs/init.el
@@ -446,5 +446,12 @@
   :ensure (:type git :host codeberg :repo "meow_king/typst-ts-mode"
                  :files (:defaults "*.el")))
 
-;;; init.el ends here
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(warning-suppress-log-types '((comp))))
+
+;;; init.el ends here
