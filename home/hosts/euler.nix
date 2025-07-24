52dfc80115bbcc6590ff775e23c1a8b126e5920c
Miles Possing <mp-complete@pm.me>
2025-07-24 09:53:28 -0500

    Remove protonmail-bridge from home-manager

--- home/hosts/euler.nix
+++ home/hosts/euler.nix
@@ -23,8 +23,6 @@
   sdev.racket.full = true;
   sdev.all = true;
 
-  services.protonmail-bridge.enable = true;
-
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
--- home/modules/default.nix
+++ home/modules/default.nix
@@ -1,10 +1,9 @@
-{ ... }: {
+{
   imports = [
     ./development
     ./emacs.nix
     ./git.nix
     ./navi
-    ./protonmail-bridge.nix
     ./shells.nix
     ./tmux.nix
     ./user-space.nix
