66d8dcd8aeb989f883832f61d2db48d99abdb102
Miles Possing <mp-complete@pm.me>
2025-07-24 09:53:42 -0500

    Update kde secret service

--- modules/wm/kde.nix
+++ modules/wm/kde.nix
@@ -1,11 +1,19 @@
 { pkgs, ... }:
 {
+  programs.kdeconnect.enable = true;
+
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
+  security.pam.services.sddm.enableGnomeKeyring = true;
   services.desktopManager.plasma6.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  services.dbus.packages = with pkgs; [ gnome-keyring ];
 
   environment.systemPackages = with pkgs; [
     kdePackages.kate
-    kdePackages.kdeconnect-kde
+    kdePackages.kwallet
+    kdePackages.kwalletmanager
+    libsecret
+    seahorse
   ];
 }
