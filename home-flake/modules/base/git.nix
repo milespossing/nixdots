7d9f1e49c2230306b2b494133f23c6ae89644ce4
Miles Possing <miles@nixos.localdomain>
2025-12-16 18:11:57 +0000

    Fix git

--- home-flake/modules/base/git.nix
+++ home-flake/modules/base/git.nix
@@ -4,7 +4,29 @@
 {
   programs.git = {
     enable = true;
-    userName = "Miles Possing";
+    settings = {
+      user.name = "Miles Possing";
+      aliases = {
+        s = "status";
+        c = "checkout";
+        d = "diff";
+      };
+      extraConfig = {
+        init.defaultBranch = "main";
+        pull.rebase = false;
+        diff.tool = "nvimdiff";
+        merge.tool = "nvimdiff";
+        http.postBuffer = 524288000;
+        mergetool = {
+          keepBackup = false;
+        };
+        push.autoSetupRemote = true;
+        core = {
+          editor = "nvim";
+          pager = "bat";
+        };
+      };
+    };
     ignores = [
       "*~"
       "*.swp"
@@ -14,26 +36,6 @@
       ".direnv"
       ".envrc"
     ];
-    aliases = {
-      s = "status";
-      c = "checkout";
-      d = "diff";
-    };
-    extraConfig = {
-      init.defaultBranch = "main";
-      pull.rebase = false;
-      diff.tool = "nvimdiff";
-      merge.tool = "nvimdiff";
-      http.postBuffer = 524288000;
-      mergetool = {
-        keepBackup = false;
-      };
-      push.autoSetupRemote = true;
-      core = {
-        editor = "nvim";
-        pager = "bat";
-      };
-    };
   };
 
   programs.gh = {
