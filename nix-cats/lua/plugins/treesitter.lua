7c5603c4e57369d6b478146509827d3b6cc597b3
Miles Possing <milespossing@microsoft.com>
2026-02-25 18:44:55 +0000

    Fix treesitter

--- nix-cats/lua/plugins/treesitter.lua
+++ nix-cats/lua/plugins/treesitter.lua
@@ -3,31 +3,23 @@ return {
     'nvim-treesitter/nvim-treesitter',
     enabled = require('nixCatsUtils').enableForCategory('treesitter'),
     build = require('nixCatsUtils').lazyAdd ':TSUpdate',
-    opts = {
-      -- NOTE: nixCats: use lazyAdd to only set these 2 options if nix wasnt involved.
-      -- because nix already ensured they were installed.
-      ensure_installed = require('nixCatsUtils').lazyAdd { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
-      auto_install = require('nixCatsUtils').lazyAdd(true, false),
+    lazy = false,
+    config = function()
+      require('nvim-treesitter').setup {}
 
-      highlight = {
-        enable = true,
-      },
-      indent = { enable = true },
-    },
-    config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+      -- Install parsers when not managed by nix
+      if require('nixCatsUtils').lazyAdd(true, false) then
+        require('nvim-treesitter').install { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' }
+      end
 
-      -- Prefer git instead of curl in order to improve connectivity in some environments
-      require('nvim-treesitter.install').prefer_git = true
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
-
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+      -- Enable treesitter-based highlighting and indentation for supported filetypes
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          if pcall(vim.treesitter.start, args.buf) then
+            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
     end,
   },
 }
